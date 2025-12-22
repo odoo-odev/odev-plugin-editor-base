9fbcb0a7a07c8dacb6459157d12b9d6a2d4d04c6
brinkflew <avs@avanserv.com>
2025-12-22 18:34:32 +0100

    [IMP] Open directories for an Odoo version
    Add support for opening a workspace containing standard Odoo modules onlyu, without the need for a linked database or repository.

--- commands/code.py
+++ commands/code.py
@@ -1,4 +1,6 @@
+from odev.common import args
 from odev.common.commands import DatabaseOrRepositoryCommand
+from odev.common.version import OdooVersion
 
 from odev.plugins.odev_plugin_editor_base.common.editor import Editor
 
@@ -9,6 +11,21 @@ class EditorCommand(DatabaseOrRepositoryCommand):
     """
 
     _name = "code"
+
+    _database_allowed_platforms = ["local"]
+
+    _exclusive_arguments = [("database", "version")]
+
+    version = args.String(
+        aliases=["-V", "--version"],
+        description="Odoo version to open a workspace for.",
+    )
+
+    @classmethod
+    def prepare_command(cls, *args, **kwargs) -> None:
+        super().prepare_command(*args, **kwargs)
+        cls.remove_argument("platform")
+        cls.remove_argument("branch")
 
     def run(self):
         editor_subclasses = Editor.__subclasses__()
@@ -21,7 +38,11 @@ class EditorCommand(DatabaseOrRepositoryCommand):
         editor_class = Editor.__subclasses__()[0]
 
         try:
-            editor = editor_class(self._database, self.args.repository)
+            editor = editor_class(
+                self._database,
+                self.args.repository,
+                OdooVersion(self.args.version) if self.args.version else None,
+            )
         except ValueError as error:
             raise self.error(str(error)) from error
 
